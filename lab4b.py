c2162e6a4b893ffd643dbada85c6c60a223beed6
Megodwin Varghese <mvarghese10@myseneca.ca>
2024-06-07 17:18:50 -0400

    Added lab4b

---
+++ lab4b.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Author: Megodwin Varghese
+
+def join_lists(l1, l2):
+    
+    return list(set(l1) | set(l2))
+
+def match_lists(l1, l2):
+    
+    return list(set(l1) & set(l2))
+
+def diff_lists(l1, l2):
+    
+    return list(set(l1) ^ set(l2))
+
+
+# Main block
+
+if __name__ == '__main__':
+    list1 = list(range(1, 10))
+    list2 = list(range(5, 15))
+    print('list1: ', list1)
+    print('list2: ', list2)
+    print('join: ', join_lists(list1, list2))
+    print('match: ', match_lists(list1, list2))
+    print('diff: ', diff_lists(list1, list2))
